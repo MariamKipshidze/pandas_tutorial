d5901af0034b90f57dda076a29569b6ccb5beeef
MariamKipshidze <mari.kifshidze@gmail.com>
2025-11-08 12:17:17 +0400

    Panda read csv

--- read_csv.py
+++ read_csv.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.read_csv('data.csv')
 
 print(df.to_string())
+print(pd.options.display.max_rows)
+
+
+pd.options.display.max_rows = 9999
+df = pd.read_csv('data.csv')
+print(df)
