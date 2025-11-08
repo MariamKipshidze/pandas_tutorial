5e1123a733fe8b09bfcc86a6f4a5c0b2d5def079
MariamKipshidze <mari.kifshidze@gmail.com>
2025-11-08 15:40:21 +0400

    Pandas matplotlib plotting

---
+++ plotting.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+df.plot()
+plt.show()
+
+df = pd.read_csv('data.csv')
+df.plot(kind = 'scatter', x = 'Duration', y = 'Calories')
+plt.show()
+
+df["Duration"].plot(kind = 'hist')
+plt.show()
